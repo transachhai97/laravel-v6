0f2e08ff64d4bc7827cd6b0a9edaabd7082f355e
HaiTS <transachhai97@gmail.com>
2020-09-20 14:22:37 +0700

    laravel module main

---
+++ Modules/Main/Database/Seeders/MainDatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Main\Database\Seeders;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
+
+class MainDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        // $this->call("OthersTableSeeder");
+    }
+}
